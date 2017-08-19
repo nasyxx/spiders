ff48b16b683e1709bb650027643ab78e0a65bc75
Nasy <sy_n@me.com>
2017-08-20 01:38:58 +0800

    Add another way

--- 0001/spider.py
+++ 0001/spider.py
@@ -14,13 +14,16 @@ Excited without bugs::
 * file: spider.py
 * license: MIT
 
-This one is used to crawl the table on this page::
+This one I used to tell my friends to use requests, rather than urlib. and It
+    is used to crawl the table on this page and sum the comments::
+
     http://py4e-data.dr-chuck.net/comments_42.html
 
 Just a simple example.
 
 Copyright © 2017 by Nasy. All Rights Reserved.
 """
+import re
 import requests as req
 import bs4
 
@@ -34,6 +37,7 @@ def main() -> None:
     html = req.get(THEURL)
     content = bs4.BeautifulSoup(html.content, "html.parser")  # or lxml
 
+    comments = 0
     # Get tr
     trs = content.select("tr")
     for tag_tr in trs:
@@ -41,6 +45,19 @@ def main() -> None:
         tag_td = tag_tr.select("td")
         name, comment = tag_td[0], tag_td[1]
         print("name:", name.text, "comment:", comment.text)
+        if comment.text == "Comments":
+            # This is the head of the table. We have to pass it
+            continue
+        else:
+            comments += int(comment.text)
+    print(comments)  # 2553
+
+    # Another way
+    table = content.select("table")
+    table_text = table[0].text
+    all_comments = re.findall("[0-9]+", table_text)
+    comments = sum(int(i) for i in all_comments)
+    print(comments)  # 2553
 
 
 if __name__ == "__main__":
